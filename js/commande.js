8b509e110d9e959ed9afc812344ae7c1ae0bd5ac
nikolaspaci <nikolaspaci>
2019-07-01 11:46:53 +0200

    BC

--- js/commande.js
+++ js/commande.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	$('[data-toggle="tooltip"]').tooltip();
-	var actions = $("table td:last-child").html();
+	var actions = $("#tablecom td:last-child").html();
 	// Append table with add row form on add new button click
     $(".add-new").click(function(){
 		$(this).attr("disabled", "disabled");
-		var index = $("table tbody tr:last-child").index();
+		var index = $("#tablecom tbody tr:last-child").index();
         var row = '<tr>' +
             '<td><input type="text" class="form-control" name="name" id="name"></td>' +
             '<td><input type="text" class="form-control" name="department" id="department"></td>' +
@@ -15,8 +15,8 @@ $(document).ready(function(){
 			'<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
 			'<td>' + actions + '</td>' +
         '</tr>';
-    	$("table").append(row);		
-		$("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
+    	$("#tablecom").append(row);		
+		$("#tablecom tbody tr").eq(index + 1).find(".add, .edit").toggle();
         $('[data-toggle="tooltip"]').tooltip();
     });
 	// Add row on add button click
